ed33f7c932de3fa4c204042b1d2475ceeab80e5f
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-09-13 20:10:55 +1000

    add fmt & lint scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,10 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: { project: ["./tsconfig.json"] },
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"]
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
 };
